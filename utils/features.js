14b8f345ca67a3d85b2715eb1b5a9b26e7b31363
JayVaghani-Dev <jayvaghanidev@gmail.com>
2023-08-17 05:44:47 +0530

    http done

--- utils/features.js
+++ utils/features.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-console.log(process.env.NODE_ENV === "Development");
 
 module.exports.sendCookies = async (user, res, message, statusCode = 200) => {
   const token = await jwt.sign({ _id: user._id }, process.env.TOKEN);
@@ -8,8 +7,10 @@ module.exports.sendCookies = async (user, res, message, statusCode = 200) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      httpOnly: false,
+      httpOnly: true,
       maxAge: 5 * 60 * 1000,
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
       success: true,
